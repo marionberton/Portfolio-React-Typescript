71452e589b434383cdfb6c9b6d866b15a69645b1
“Marion <marionberton03@gmail.com>
2020-07-01 11:57:34 +0100

    setting up vscode

--- src/shared/sidebar.tsx
+++ src/shared/sidebar.tsx
@@ -4,9 +4,9 @@ import logo from '../assets/logo.svg';
 
 export const Sidebar: React.FC = () => {
   return (
-    <div className="h-full fixed top-0 left-0 overflow-x-hidden w-20 z-10 pt-5">
+    <div className="h-full fixed top-0 left-0 overflow-x-hidden w-1/3 z-10 pt-5 bg-bgOrange">
       <div>
-        <img src={logo} alt="logo" className="mx-auto w-10" />
+        <img src={logo} alt="logo" className="pl-5" />
       </div>
     </div>
   );
